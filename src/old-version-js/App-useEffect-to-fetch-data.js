c90f7ecc8b7a9831cf81614c24485d5711bf4aa0
fengbanjiazhu <562475805@qq.com>
2023-02-18 19:18:36 +1100

    fetch redux data with useEffect

--- src/old-version-js/App-useEffect-to-fetch-data.js
+++ src/old-version-js/App-useEffect-to-fetch-data.js
@@ -1,11 +1,11 @@
 import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 
-import Cart from "./components/Cart/Cart";
-import Layout from "./components/Layout/Layout";
-import Products from "./components/Shop/Products";
-import { uiActions } from "./store/ui-slice";
-import Notification from "./components/UI/Notification";
+import Cart from "../components/Cart/Cart";
+import Layout from "../components/Layout/Layout";
+import Products from "../components/Shop/Products";
+import { uiActions } from "../store/ui-slice";
+import Notification from "../components/UI/Notification";
 
 let isInitial = true;
 
